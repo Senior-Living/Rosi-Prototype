2380a91fc21c80373ca64c71a725c5857e7136d7
Ishan Phadte <ishanphadte@gmail.com>
2024-09-08 23:24:10 -0400

    wip

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template, request
+from pymongo import MongoClient
 
 app = Flask(__name__)
+
+# Connect to local MongoDB
+client = MongoClient('mongodb://localhost:27017/')
+db = client['local']  # Database name
+collection = db['ROSIUserConn']  # Collection name
 
 @app.route('/')
 def home():
@@ -35,6 +41,20 @@ def submit():
     print(f"City: {city}")
     print(f"Language: {language}")
     print(f"Personality Type: {personality}")
+
+    submission_data = {
+        "firstname": firstname,
+        "lastname": lastname,
+        "age": age,
+        "gender": gender,
+        "province": province,
+        "city": city,
+        "language": language,
+        "personality": personality
+    }
+
+    collection.insert_one(submission_data)  # Insert data into MongoDB collection
+
 
     return f"Form submitted successfully!<br>First Name: {firstname}<br>Last Name: {lastname}<br>Age: {age}<br>Gender: {gender}<br>Province: {province}<br>City: {city}<br>Language: {language}<br>Personality Type: {personality}"
 
